449006625db00bd5148bf011358439365ad5686d
Linus Wæhler <linus.wahler@gmail.com>
2025-02-06 02:30:44 +0100

    feat: Yank from visual mode

--- lua/fencely/init.lua
+++ lua/fencely/init.lua
@@ -1,8 +1,22 @@
 local M = {}
 
-M.copy_as_fence = function()
+M.config = {
+  highlight = false, -- TODO: Implement
+}
+
+M.copy_as_fence = function(start_line, end_line)
   local ft = vim.bo.filetype or ''
-  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local buf = vim.api.nvim_get_current_buf()
+
+  if not start_line or not end_line then
+    start_line = 0
+    end_line = vim.api.nvim_buf_line_count(buf)
+  end
+
+  -- Adjust for 1-indexing
+  start_line = start_line - 1
+
+  local lines = vim.api.nvim_buf_get_lines(buf, start_line, end_line, false)
   local md = { '```' .. ft }
   for _, line in ipairs(lines) do
     table.insert(md, line)
@@ -10,13 +24,18 @@ M.copy_as_fence = function()
   table.insert(md, '```')
   local md_str = table.concat(md, '\n')
   vim.fn.setreg('+', md_str)
-  print 'Copied as a Markdown code block!'
+
+  if #lines > 1 then
+    print(#lines .. ' lines fence yanked')
+  end
 end
 
-function M.setup(_)
-  vim.api.nvim_create_user_command('FenceYank', function()
-    M.copy_as_fence()
-  end, { desc = 'Copy as a fence' })
+function M.setup(config)
+  M.config = vim.tbl_deep_extend('force', M.config, config or {})
+
+  vim.api.nvim_create_user_command('FenceYank', function(opts)
+    M.copy_as_fence(opts.line1, opts.line2)
+  end, { desc = 'Copy as a fence', range = true })
 end
 
 return M
